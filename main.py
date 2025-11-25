faeb023d1ff88dc19434aad945d2e48496422694
CatsEleven <hatijolove@gmail.com>
2025-11-25 17:00:55 +0900

    大人は完成

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.route('/link')
+
+@app.route('/')
 def index():
-    return render_template('adult.html', name='Flask Beginner')
+    response = supabase.table("near-miss-log").select("*").execute()
+    logs = response.data  # list of dict
+
+    return render_template("adult.html", logs=logs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
