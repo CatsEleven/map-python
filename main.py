432d3c05f7eb6c8ec689987c4e7c7f231c8c8cac
CatsEleven <hatijolove@gmail.com>
2025-11-25 16:27:55 +0900

    supabase手前

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from map-python!")
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+@app.route('/link')
+def index():
+    return render_template('adult.html', name='Flask Beginner')
+
+if __name__ == '__main__':
+    app.run(debug=True)
