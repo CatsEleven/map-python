70a479f35608241c80f06998d4b0011b56d2ddd2
CatsEleven <hatijolove@gmail.com>
2025-11-25 19:26:16 +0900

    スーパー追加

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/adult')
+def adult():
     response = supabase.table("near-miss-log").select("*").execute()
-    logs = response.data  # list of dict
+    logs = response.data  
 
     return render_template("adult.html", logs=logs)
 
 
+@app.route('/child')
+def child():
+    response = supabase.table("near-miss-log").select("*").execute()
+    logs = response.data  
+
+    return render_template("child.html", logs=logs)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
